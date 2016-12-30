c1698ad3fe4e0369675086597a73f3eae7ca328e
Mehul Kumar <compcode18@gmail.com>
2016-12-31 03:04:31 +0530

    weather is working

--- apis.py
+++ apis.py
@@ -3,17 +3,38 @@ import json
 import sys
 
 def fetch_weather(city):
+    pp = pprint.PrettyPrinter(indent=4)
+
+    #get your key from http://api.openweathermap.org/
+    API_KEY = "key"
+
+
+    CITY_NAME = city
+
+
+
     try:
-        u = "https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22{0}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
-        r = requests.get(u.format(city))
-        r = requests.get(u.format("North Pole"))
-        j = json.loads(r.text)
-        results = j['query']['results']['channel']['item']
-        return results
-    except Exception as e:
-        print(e)
-        sys.stderr.write("Couldn't load current conditions\n")
-        return "Unkonw" # typo?
+        f = urllib2.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' \
+                        + CITY_NAME + '&units=metric' + '&APPID=' +  API_KEY)
+        respond_raw = f.read()
+    except:
+            print 'Execption: URL is not correct!'
+            raise
+
+
+
+    weather_dict = json.loads(respond_raw)
+
+
+
+    #pp.pprint(weather_dict)
+
+
+
+    print('\n\n' + weather_dict['name']+" is "+ \
+          weather_dict['weather'][0]['description'] + \
+          ', temperature: ' + str(weather_dict['main']['temp']) + \
+          ', humidity: ' + str(weather_dict['main']['humidity']) + '%.' )
 
 
 def fetch_stocks(ticker):
